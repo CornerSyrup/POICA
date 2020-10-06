cb1a702dca3d2b54013086aca30cb858cd362575
THS95049 <ths95049@outlook.com>
2020-10-06 13:25:33 +0900

    Rename verifier to validator

--- www/model/Validator.php
+++ www/model/Validator.php
@@ -2,14 +2,14 @@
 
 namespace model;
 
-class Verifier
+class Validator
 {
-    public static function verify_sid(string $sid): bool
+    public static function validate_sid(string $sid): bool
     {
         return preg_match('/^\d+$/', $sid);
     }
 
-    public static function verify_pwd(string $password): bool
+    public static function validate_pwd(string $password): bool
     {
         return preg_match('/^*+$/', $password);
     }
