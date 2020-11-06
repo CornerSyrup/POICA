775204971ae20eeff9d72008b8140d1df57bb3eb
THS95049 <ths95049@outlook.com>
2020-11-06 15:07:48 +0900

    :sparkles: Implement new exception for record not found
    implement RecordNotFoundException; and
    updated related use cases

--- www/model/DBAdaptor.php
+++ www/model/DBAdaptor.php
@@ -47,13 +47,35 @@ class DBAdaptor
     public static function obtain_credential(string $sid): string
     {
         $con = self::create_connection();
-        $res = pg_fetch_array(pg_query($con, "SELECT Usership.obtain_pwd('${sid}');"));
+        try {
+            $res = pg_fetch_array(pg_query($con, "SELECT Usership.obtain_pwd('${sid}');"));
+        } catch (\Throwable $th) {
+            throw new RecordNotFoundException('Fail to find record for database access error', 0, $th);
+        }
 
         if (empty($res[0])) {
-            throw new Exception('credential not found');
+            throw new RecordNotFoundException("Fail to obtain credential with student ID ${sid}");
         }
 
         pg_close($con);
         return $res[0];
+    }
+}
+
+/**
+ * Exception representing record not found in database
+ */
+class RecordNotFoundException extends Exception
+{
+    /**
+     * Constructor of record not found exception
+     *
+     * @param string $message
+     * @param integer $code
+     * @param Exception $innerException
+     */
+    public function __construct(string $message = '', int $code = 0, Exception $innerException = null)
+    {
+        parent::__construct($message, $code, $innerException);
     }
 }
