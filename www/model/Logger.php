9c429dd1304d27f3a94dde2b73fca553eb4b13af
THS95049 <ths95049@outlook.com>
2020-11-10 12:07:55 +0900

    :recycle: Rework on logger
    Test logger to mkdir for logs; and
    success append new log; and
    provide error logging function

--- www/model/Logger.php
+++ www/model/Logger.php
@@ -2,9 +2,14 @@
 
 namespace model;
 
+use Throwable;
+
+require 'Global.php';
+
 class Logger
 {
     public $tag;
+    public $dirPath;
     public $fileName;
 
     /**
@@ -13,30 +18,49 @@ class Logger
      * @param string $tag tag for the log record.
      * @param string $file file name of the log without extension.
      */
-    public function __construct(string $tag = 'Event', string $file = 'general')
+    public function __construct(string $tag, string $file = null)
     {
         $this->tag = $tag;
-        $this->fileName = $file;
+        $this->dirPath = join_path(dirname(__DIR__), 'logs');
+        $this->fileName = is_null($file) ? $tag . '.log' : $file . '.log';
+
+        if (!file_exists($this->dirPath)) {
+            mkdir($this->dirPath);
+        }
     }
 
     /**
-     * Append a new record to log file with given detail string.
+     * Append a new record to log file with given message string.
      *
-     * @param string $detail body part of the log record.
-     * @return boolean true on success put; false on failure.
+     * @param string $msg body part of the log record.
+     * @return boolean true on success append; false on failure.
      */
-    public function appendRecord(string $detail): bool
+    public function appendRecord(string $msg): bool
     {
-        $dirPath = '../logs/';
-
-        if (!file_exists($dirPath)) {
-            mkdir($dirPath);
-        }
-
         if (!empty($this->tag)) {
-            $detail = "{$this->tag}\t{$detail}";
+            $msg = "[{$this->tag}]\t{$msg}";
         }
 
-        return (bool) file_put_contents("{$dirPath}/{$this->fileName}.log", $detail . PHP_EOL, FILE_APPEND);
+        return $this->append($msg);
+    }
+
+    /**
+     * Append a new record of error to log file with given exception instance.
+     *
+     * @param Throwable $error instance of the exception.
+     * @return boolean true on success append; false on failure.
+     */
+    public function appendError(Throwable $error): bool
+    {
+        $msg = empty($this->tag) ?
+            "Error:\t{$error->getMessage()}" :
+            "[{$this->tag}] Error:\n{$error->getMessage()}";
+
+        return $this->append($msg);
+    }
+
+    private function append(string $data): bool
+    {
+        return (bool) file_put_contents(join_path($this->dirPath, $this->fileName), $data . PHP_EOL, FILE_APPEND);
     }
 }
