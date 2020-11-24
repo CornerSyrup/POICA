bd349f8df3366beb97e81e6e00d513b2154c250f
THS95049 <ths95049@outlook.com>
2020-11-24 16:30:21 +0900

    Create request method exception
    for inappropriate http request

--- www/model/Global.php
+++ www/model/Global.php
@@ -24,3 +24,38 @@ function get_server_root(): string
 {
     return dirname(__DIR__);
 }
+
+/**
+ * Exception representing inappropriate http request method.
+ */
+class RequestMethodException extends \Exception
+{
+    /**
+     * Actual http method received.
+     *
+     * @var string
+     */
+    public $actual;
+    /**
+     * Expected http method.
+     *
+     * @var string
+     */
+    public $expected;
+
+    /**
+     * Constructor of request method exception.
+     *
+     * @param string $expected expected request method.
+     * @param string $actual actual request method received.
+     * @param integer $code exception code for exception instance.
+     * @param \Exception $innerException inner exception of instance.
+     */
+    public function __construct(string $expected, string $actual, int $code = 0, \Exception $innerException = null)
+    {
+        $this->actual = $actual;
+        $this->expected = $expected;
+
+        parent::__construct("Request method expected [{$expected}] instead of [{$actual}].", $code, $innerException);
+    }
+}
