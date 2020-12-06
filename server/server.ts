1b6190431b3c8541ad534faf616d794066e7491d
THS95049 <ths95049@outlook.com>
2020-12-07 01:22:55 +0900

    :sparkles: Create mailing service route
    with mock mail server, powered by MailTrap;

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import nodemailer, { TestAccount } from "nodemailer";
+
+import MailAccount from "./MailAccount";
 
 // general config
 const PORT = 80;
@@ -13,6 +16,52 @@ server.get("/auth/", (req, res) => {
 
   res.header("Content-Type", "application/json");
   res.send(JSON.stringify(respond));
+});
+//#endregion
+
+//#region mailing service
+const mailer = nodemailer.createTransport({
+  // dev env with MailTrap
+  host: "smtp.mailtrap.io",
+  port: 587 ,
+  auth: {
+    user: "142de77bed440f",
+    pass: "a12f9f2c00546c",
+  },
+  pool: true,
+  maxConnections: 1,
+});
+
+const sender: MailAccount = new MailAccount(
+  "IH12A092 Group 5",
+  "service@group5-ih12a092.com"
+);
+
+server.post("/mail/", (req, res) => {
+  mailer
+    .sendMail({
+      from: sender.AccountString,
+      to: new MailAccount("Manager", "manager@tokyo.hal.ac.jp").AccountString,
+      text: "text body",
+      html: "<html><body><p>HTML body</p></body></html>",
+    })
+    .then(
+      (info) => {
+        console.log(`Mail sent Successfully, with sending info:`);
+        console.log(JSON.stringify(info, null, 2));
+      },
+      (reason) => {
+        console.warn(`Mail sent Failingly, with reason:`);
+        console.log(JSON.stringify(reason, null, 2));
+      }
+    )
+    .catch((reason) => {
+      console.error(`Mail sent Failingly, with error:`);
+      console.log(JSON.stringify(reason, null, 2));
+    });
+
+  res.statusCode = 200;
+  res.send();
 });
 //#endregion
 
