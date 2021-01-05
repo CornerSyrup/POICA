2379621ff3912e9087fd5ca41a87a88b45063fbc
THS95049 <ths95049@outlook.com>
2021-01-05 18:58:45 +0000

    :fire: Remove procedual version

--- www/controller/apply/apply_get.php
+++ www/controller/apply/apply_get.php
@@ -17,22 +17,6 @@ namespace controller\apply;
 
 require_once 'model/DBAdaptor.php';
 require_once 'model/Logger.php';
-
-use model;
-
-$logger->SetTag('get');
-
-try {
-    $dba = new model\DBAdaptor();
-    $res['frm'] = $dba->obtain_form($_REQUEST['id'], $_SESSION['user']);
-    $logger->appendRecord("[{$_SESSION['user']}] obtained application form entry [{$_REQUEST['id']}].");
-    $res['status'] = 1;
-} catch (model\RecordNotFoundException $rnf) {
-    $logger->appendError($rnf);
-    $res['status'] = -3;
-    $res['error'] = 'Entry not found in database';
-}
-
 require_once 'model/Handler.php';
 
 class GetHandler extends \model\GetHandler
@@ -45,11 +29,11 @@ class GetHandler extends \model\GetHandler
      */
     public function Handle(): array
     {
-        $dba = new model\DBAdaptor();
+        $dba = new \model\DBAdaptor();
 
         $this->respond['frm'] = $dba->obtain_form($this->data['id'], $_SESSION['user']);
         $this->respond['status'] = 1;
-        
+
         $this->logger->appendRecord("[{$_SESSION['user']}] obtained application form entry [{$this->data['id']}].");
 
         return $this->respond;
