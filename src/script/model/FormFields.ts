6c52cee46069174ef9f721c34411970a9de0db32
THS95049 <ths95049@outlook.com>
2021-02-02 22:56:56 +0000

    Create definition of common fields
    Interface for common fields in all applications.
    Used to match react hook form, etc.

---
+++ src/script/model/FormFields.ts
@@ -0,0 +1,30 @@
+export interface CommonFields {
+    /**
+     * First name, given name.
+     */
+    fn: string;
+    /**
+     * Fist name kana.
+     */
+    fk: string;
+    /**
+     * Last name, family name.
+     */
+    ln: string;
+    /**
+     * Last name kana.
+     */
+    lk: string;
+    /**
+     * Student ID.
+     */
+    si: number;
+    /**
+     * Class Code.
+     */
+    cc: string;
+    /**
+     * Class Teacher.
+     */
+    ct: number;
+}
