291f05d4322527d01a80a1e12a2285a2a335d41f
THS95049 <ths95049@outlook.com>
2021-02-03 03:30:57 +0000

    Simplify code
    function will call whenever render called

--- src/script/page/insider/form/doc_issue/progress.tsx
+++ src/script/page/insider/form/doc_issue/progress.tsx
@@ -10,15 +10,15 @@ export default class Progress extends React.Component<Props> {
     stepState = (step: number) => {
         let current = Number.parseInt(this.props.match.params.step);
         return (
-            (current > step ? "completed" : current == step ? "active" : "") +
-            " "
+            (current > step ? "completed " : current == step ? "active " : "") +
+            "step"
         );
     };
 
     render() {
         return (
             <div id="progress" className="ui four ordered steps">
-                <div className={this.stepState(1) + "step"}>
+                <div className={this.stepState(1)}>
                     <div className="content">
                         <div className="title">基本情報</div>
                         <div className="description">
@@ -28,17 +28,17 @@ export default class Progress extends React.Component<Props> {
                         </div>
                     </div>
                 </div>
-                <div className={this.stepState(2) + "step"}>
+                <div className={this.stepState(2)}>
                     <div className="content">
                         <div className="title">申し込み書を記入</div>
                     </div>
                 </div>
-                <div className={this.stepState(3) + "step"}>
+                <div className={this.stepState(3)}>
                     <div className="content">
                         <div className="title">入力情報を確認</div>
                     </div>
                 </div>
-                <div className={this.stepState(4) + "step"}>
+                <div className={this.stepState(4)}>
                     <div className="content">
                         <div className="title">支払い</div>
                         <div className="description">手続き料を支払う</div>
