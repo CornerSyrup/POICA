6e5ac499178fc5df2bc3ba4b3dcc102a9886e50d
THS95049 <ths95049@outlook.com>
2021-02-07 11:29:39 +0000

    Create extended interface for student
    Base student interface will be the most common collection
    that the server respond.
    Which this extended version will be useful
    when any suica related request made.

--- src/script/model/student.ts
+++ src/script/model/student.ts
@@ -3,3 +3,10 @@ export interface BasicStudent {
     fname: string;
     lname: string;
 }
+
+export interface SuicaStudent extends BasicStudent {
+    /**
+     * IDm code of the suica card registered by the student.
+     */
+    suica: string;
+}
