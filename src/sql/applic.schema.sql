b55c01e154a18a6ddb81741ac9d7ba176b1ff602
THS95049 <ths95049@outlook.com>
2021-02-11 01:34:52 +0000

    Rework on application

--- src/sql/applic.schema.sql
+++ src/sql/applic.schema.sql
@@ -7,21 +7,22 @@ CREATE TABLE Applic.Applications (
 	-- status of application handling
 	stat BIT(2) DEFAULT B'00' NOT NULL,
 	applyUser INT NOT NULL,
-	applyDate DATE NOT NULL DEFAULT NOW(),
-	formData JSON,
+	applyDate DATE NOT NULL DEFAULT CURRENT_DATE,
+	formData JSON NOT NULL,
+	formType CHAR(6) NOT NULL,
 	CONSTRAINT PK_Application PRIMARY KEY (appID),
 	CONSTRAINT FK_App_Student FOREIGN KEY (applyUser) REFERENCES Usership.Users (userID)
 );
 
-CREATE TABLE Applic.Prefill (
+CREATE TABLE Applic.Prefills (
 	entryID SERIAL,
 	userID INT NOT NULL enFName VARCHAR(30),
 	enLName VARCHAR(30),
 	birthDay DATE,
 	-- true for male, false for female
 	gender BIT(1),
-	-- triple size for ja, 100 char
-	addr VARCHAR(300),
+	-- triple size for ja, 150 char
+	addr VARCHAR(450),
 	-- graduate year and month, for graduates
 	gradMonth DATE,
 	phone CHAR(10),
