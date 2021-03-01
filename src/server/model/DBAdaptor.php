9d9fad8c122fe32747fbd03476402ad692007e52
THS95049 <ths95049@outlook.com>
2021-03-01 21:09:54 +0000

    Simplify func with doc

--- src/server/model/DBAdaptor.php
+++ src/server/model/DBAdaptor.php
@@ -2,7 +2,7 @@
 
 namespace POICA\model {
 
-    use POICA\model\exception\RecordLookUpException;
+    use POICA\model\exception as ex;
 
     /**
      * Database related service provider.
@@ -39,25 +39,32 @@ namespace POICA\model {
             }
         }
 
+        /**
+         * Query to database.
+         *
+         * @param string $command Query command with param placeholder.
+         * @param array $params Array of params.
+         * @param string $errMsg Message to be used as exception when thrown.
+         * @param boolean $assoc Whether to return data in associative array.
+         * @throws ex\RecordLookUpException throws when error encountered on query procedure.
+         * @throws ex\RecordNotFoundException throws when record not found in database.
+         */
         public function query(string $command, array $params, string $errMsg, bool $assoc = true)
         {
             $res = @pg_query_params($this->connection, $command, $params);
 
-            if (!$res) {
-                throw new RecordLookUpException($errMsg, 0, new \Exception(pg_errormessage($this->connection)));
-            } else {
-                if ($assoc)
-                    $res = pg_fetch_all($res);
-                else
-                    $res = pg_fetch_all($res, PGSQL_NUM);
-            }
+            if (!$res)
+                throw new ex\RecordLookUpException($errMsg, 0, new \Exception(pg_errormessage($this->connection)));
+            else
+                // false on no row
+                $res = $assoc
+                    ? pg_fetch_all($res, PGSQL_ASSOC)
+                    : pg_fetch_all($res, PGSQL_NUM);
 
-            if (!$res) {
-                $msg = pg_errormessage($this->connection);
-                if (empty($msg))
-                    $msg = 'No row fetched or other error encountered.';
-                throw new RecordLookUpException($errMsg, 0, new \Exception($msg));
-            }
+            if (!$res)
+                throw new ex\RecordNotFoundException($errMsg, 0, new \Exception(
+                    'No row return from database.'
+                ));
 
             return $res;
         }
