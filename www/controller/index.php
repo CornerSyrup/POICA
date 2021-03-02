bcd3d62ec41fa5ff080e561ce639aefac38c1c54
THS95049 <ths95049@outlook.com>
2021-03-02 21:44:20 +0000

    Complete page request handler
    Replace page name with placeholder

--- www/controller/index.php
+++ www/controller/index.php
@@ -10,13 +10,15 @@ ob_start();
 if (isset($_SESSION['log_in']) && $_SESSION['log_in']) {
     // user is student.
     if (isset($_SESSION['sid']))
-        include __DIR__ . '/../view/sign.html';
+        include __DIR__ . '/../view/student.html';
     // user is teacher.
     elseif (isset($_SESSION['tid']))
-        include __DIR__ . '/../view/sign.html';
+        include __DIR__ . '/../view/teacher.html';
 }
 // not logged in.
 else
     include __DIR__ . '/../view/sign.html';
 
 ob_end_flush();
+
+exit;
